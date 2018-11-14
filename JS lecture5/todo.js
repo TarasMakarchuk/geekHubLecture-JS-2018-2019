e00a0fa58bf8fb52ceb333fa02da1d834d9c9f5e
TarasMakarchuk <2278452@gmail.com>
2018-11-14 23:04:25 +0200

    #geekHub lecture 5

---
+++ JS lecture5/todo.js
@@ -0,0 +1,54 @@
+
+window.onload = function () {
+
+    var arrTodo = [];
+
+    document.getElementById("add").onclick = function () {
+        var inputValue = document.getElementById("in").value;
+        var temp = {};
+        temp.todo = inputValue;
+        temp.check = false;
+        arrTodo[arrTodo.length] = temp;
+        console.log(arrTodo);
+        outputValues();
+    };
+
+        document.body.style.backgroundColor = "yellow";
+
+    function outputValues() {
+        var output = "";
+        for (var key in arrTodo) {
+            if (arrTodo[key].check === true) {
+                output += "<input type='checkbox' checked>";
+            } else {
+                output += "<input type='checkbox'>";
+            }
+            output += arrTodo[key].todo + "<br>";
+        }
+        document.getElementById("out").innerHTML = output;
+
+    }
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
