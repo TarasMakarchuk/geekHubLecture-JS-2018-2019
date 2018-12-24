0c1f13c495d57d7b923422ec6b363a8e92e3cb2d
TarasMakarchuk <43746781+TarasMakarchuk@users.noreply.github.com>
2018-12-24 17:54:12 +0200

    Update scripSlider.js

--- JS Lecture8 (game tag, slider)/slider/scripSlider.js
+++ JS Lecture8 (game tag, slider)/slider/scripSlider.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
     let widthSlide = 500;
-    $('.slider-wrapper').width($('.slider-wrapper').children().size() * widthSlide);
+    let slider = $('.slider-wrapper');
+    slider.width(slider.children().size() * widthSlide);
     setInterval(nextSlide, 1500);
 
     function nextSlide() {
-        let curentSlide = parseInt($('.slider-wrapper').data('current'));
+        let curentSlide = parseInt(slider.data('current'));
         curentSlide++;
-        if (curentSlide >= $('.slider-wrapper').children().size()) {
+        if (curentSlide >= slider.children().size()) {
             curentSlide = 0;
         }
-        $('.slider-wrapper').animate({left: -curentSlide * widthSlide}, 600).data('current', curentSlide);
+        slider.animate({left: -curentSlide * widthSlide}, 600).data('current', curentSlide);
     }
 });
