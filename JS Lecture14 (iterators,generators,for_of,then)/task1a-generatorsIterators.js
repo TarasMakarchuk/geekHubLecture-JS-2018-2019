77472ce8ea820c737b786733766dbe0a093895ff
TarasMakarchuk <43746781+TarasMakarchuk@users.noreply.github.com>
2019-02-01 16:59:30 +0200

    #geekHub JS Lecture14 (iterators, generators, for_of, then)

--- JS Lecture14 (iterators,generators,for_of,then)/task1a-generatorsIterators.js
+++ JS Lecture14 (iterators,generators,for_of,then)/task1a-generatorsIterators.js
@@ -9,18 +9,18 @@ setInterval(function () {
     let start = 1;
     let end = 5;
     let bitDepth = 1000000000000000000;
-    let generator = generateSequence();
+    let generator = generateRandomNumbers();
     let next = generator.next();
 
 //#######################################################################
 
-    function* generateSequence() {
+    function* generateRandomNumbers() {
         for (let i = start; i <= end; i++)
             yield array.push(Math.random() * bitDepth) / (end - start);
     }
 
     console.log('generated id: ' + array);
-    console.log(JSON.stringify(next));
+    // console.log(JSON.stringify(next));
 }, 800);
 
 //#######################################################################
